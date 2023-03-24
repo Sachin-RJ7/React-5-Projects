bbf7199edaddc16566a7c311526d845b543162ba
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2023-03-24 20:36:29 +0500

    Added routing functionality

--- One/src/App.jsx
+++ One/src/App.jsx
@@ -1,12 +1,40 @@
-import Header from './components/Header'
-import "./styles/App.scss";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import About from './components/About';
+import { createBrowserRouter,Outlet, RouterProvider} from 'react-router-dom';
+import Header from './components/Header';
+import Home from './components/Home';
 
-const App = () => {
-  return (
-    <>
-      <Header />
-    </>
-  )
+import './styles/app.scss';
+import './styles/header.scss';
+import './styles/home.scss';
+
+const AppLayout = () => {
+    return (
+      <>
+        <Header />
+        <Outlet />
+      </>
+    )
 }
 
-export default App;
+const AppRouter = createBrowserRouter([
+    {
+        path : '/',
+        element : <AppLayout />,
+        children : [
+            {
+                path : '/',
+                element : <Home />,
+            }
+        ]
+    },
+    {
+        path : '/about',
+        element : <About />,
+    },
+])
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<RouterProvider router = {AppRouter} />);
