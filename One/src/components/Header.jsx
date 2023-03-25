363d9791dfef1f6d443dff4a1e3f1b739d7f5b8a
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2023-03-25 17:32:11 +0500

    Added Hashlink for moving without anchor tag

--- One/src/components/Header.jsx
+++ One/src/components/Header.jsx
@@ -1,15 +1,17 @@
 import "../styles/header.scss";
 import { Link } from "react-router-dom";
+import { HashLink } from 'react-router-hash-link';
+
 const Header = () => {
     return (
         <nav>
             <h1>TechyStar.</h1>
             <main>
-                <Link to={"/"} >Home</Link> 
-                <Link to={"/"} >Contact</Link> 
-                <Link to={"/"} >About</Link> 
-                <Link to={"/"} >Brands</Link> 
-                <Link to={"/"} >Services</Link> 
+                <HashLink to={"/#home"} >Home</HashLink> 
+                <Link to={"/contact"} >Contact</Link> 
+                <HashLink to={"/#about"} >About</HashLink> 
+                <HashLink to={"/#brands"} >Brands</HashLink> 
+                <Link to={"/services"} >Services</Link> 
             </main>
         </nav>
     )
