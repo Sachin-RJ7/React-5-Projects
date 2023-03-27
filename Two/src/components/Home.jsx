8c6749e7d2a52c7398fe63c694b622594a661462
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2023-03-28 01:29:03 +0500

    initial commit

---
+++ Two/src/components/Home.jsx
@@ -0,0 +1,12 @@
+import { Button } from '@chakra-ui/react'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Home page</h1>
+    </div>
+  )
+}
+
+export default Home
